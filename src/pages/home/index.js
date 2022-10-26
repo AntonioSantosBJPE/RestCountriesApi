9e744dc89a2cc3bdf83931304b7240f5b918e56a
Antonio <netoifpe@gmail.com>
2022-10-26 20:11:32 -0300

    create Get requests api

---
+++ src/pages/home/index.js
@@ -0,0 +1,8 @@
+import {apiGetCountriesByAll,apiGetCountriesByPartialName} from "../../scripts/api.js"
+import {apiGetCountriesByFullName,apiGetCountriesByRegion} from "../../scripts/api.js"
+import {apiGetCountriesByCurrency,apiFilterAllNamesOfCountries} from "../../scripts/api.js"
+
+
+
+
+
